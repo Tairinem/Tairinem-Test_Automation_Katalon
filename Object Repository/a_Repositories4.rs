8569f351509546a2262ab5f4988ea19d2058def5
Tairine <t.mousinho.de.lima@avanade.com>
2020-03-28 17:46:05 -0300

    first commit with consult of repository Github

---
+++ Object Repository/a_Repositories4.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Repositories4</name>
+   <tag></tag>
+   <elementGuidId>4cea670d-2420-4198-bd4b-8a4c65b72732</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//main[@id='js-pjax-container']/div/div[2]/nav/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>menu-item selected</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/search?q=Sysemm&amp;type=Repositories</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Repositories4</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;js-pjax-container&quot;)/div[@class=&quot;container-lg px-md-2 mt-lg-4 clearfix&quot;]/div[@class=&quot;col-12 col-md-3 float-left px-md-2&quot;]/nav[@class=&quot;menu border d-none d-md-block&quot;]/a[@class=&quot;menu-item selected&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//main[@id='js-pjax-container']/div/div[2]/nav/a</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[4]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Sign up'])[2]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='?'])[1]/preceding::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Repositories']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/search?q=Sysemm&amp;type=Repositories')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//nav/a</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/div_Why GitHub                             _f1491f.rs
@@ -0,0 +1,304 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Why GitHub                             _f1491f</name>
+   <tag></tag>
+   <elementGuidId>ca70f912-4ca9-41a4-bec8-87b496f83121</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Sign up'])[1]/following::div[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>HeaderMenu HeaderMenu--logged-out position-fixed top-0 right-0 bottom-0 height-fit position-lg-relative d-lg-flex flex-justify-between flex-items-center flex-auto</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      
+        
+          
+        
+      
+
+        
+          
+              
+                
+                  
+                    Why GitHub?
+                    
+                      
+                    
+                  
+                  
+                    Features →
+                    
+                      Code review
+                      Project management
+                      Integrations
+                      Actions
+                          Packages
+                      Security
+                      Team management
+                      Hosting
+                    
+
+                    
+                      Customer stories →
+                      Security →
+                    
+                  
+                
+              
+              
+                Enterprise
+              
+
+              
+                
+                  
+                    Explore
+                    
+                      
+                    
+                  
+
+                  
+                    
+                      Explore GitHub →
+                    
+
+                    Learn &amp; contribute
+                    
+                      Topics
+                        Collections
+                      Trending
+                      Learning Lab
+                      Open source guides
+                    
+
+                    Connect with others
+                    
+                      Events
+                      Community forum
+                      GitHub Education
+                    
+                  
+                
+              
+
+              
+                Marketplace
+              
+
+              
+                
+                  
+                    Pricing
+                    
+                       
+                    
+                  
+
+                  
+                    Plans →
+
+                    
+                      Compare plans
+                      Contact Sales
+                    
+
+                    
+                      Nonprofit →
+                      Education →
+                    
+                  
+                
+              
+          
+        
+
+      
+          
+            
+  
+    
+      
+        
+          
+          
+            
+
+            
+              
+
+  
+
+
+  
+    
+      
+      
+      
+    
+
+    
+
+    
+    
+
+    
+      
+        Search
+      
+      
+        All GitHub
+      
+      ↵
+    
+
+    
+      Jump to
+      ↵
+    
+  
+
+
+
+
+
+  
+    No suggested jump to results
+  
+
+
+
+  
+
+
+  
+    
+      
+      
+      
+    
+
+    
+
+    
+    
+
+    
+      
+        Search
+      
+      
+        All GitHub
+      
+      ↵
+    
+
+    
+      Jump to
+      ↵
+    
+  
+
+
+  
+
+
+  
+    
+      
+      
+      
+    
+
+    
+
+    Tairine
+
+    
+      
+        Search
+      
+      
+        All GitHub
+      
+      ↵
+    
+
+    
+      Jump to
+      ↵
+    
+  
+
+
+
+
+
+            
+      
+  
+
+
+          
+
+        
+          Sign in
+        
+          
+            Sign up
+          
+      
+    </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;logged-out env-production page-responsive intent-mouse&quot;]/div[@class=&quot;position-relative js-header-wrapper&quot;]/header[@class=&quot;Header-old header-logged-out js-details-container Details position-relative f4 py-2&quot;]/div[@class=&quot;container-lg d-lg-flex flex-items-center p-responsive&quot;]/div[@class=&quot;HeaderMenu HeaderMenu--logged-out position-fixed top-0 right-0 bottom-0 height-fit position-lg-relative d-lg-flex flex-justify-between flex-items-center flex-auto&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Sign up'])[1]/following::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Skip to content'])[1]/following::div[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div[2]</value>
+   </webElementXpaths>
+</WebElementEntity>
